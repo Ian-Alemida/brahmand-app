25294e840d1eb452d3ab285bf1b333f6a3f01e20
Ian Almeida <iangtaonline1@gmail.com>
2024-03-12 14:52:22 -0300

    Criando ocmponente do Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import styled from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);;
   width: 100%;
   min-height: 100vh;
+  margin-right: 60px;
+`
+const FlexContaienr = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 42px;
 `
 
 function App() {
@@ -15,7 +22,13 @@ function App() {
     <FundoGradiente>
       <EstilosGlobais/>
       <Cabecalho/>
-      <BarraLateral/>
+      <FlexContaienr>
+        <BarraLateral/>
+        <Banner 
+          texto={'A galeria mais completa de fotos do espaço!'}
+          backgroundImage={'/imagens/Banner.png'}
+        />
+      </FlexContaienr>
     </FundoGradiente>
   )
 }
