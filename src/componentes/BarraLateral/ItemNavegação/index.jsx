d0a9f856fe600fa21383b142fdede53ffffacc81
Ian Almeida <iangtaonline1@gmail.com>
2024-03-12 14:20:56 -0300

    Terminando barra lateral

--- src/componentes/BarraLateral/ItemNavegação/index.jsx
+++ src/componentes/BarraLateral/ItemNavegação/index.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components"
 
 const ItemNavegacaoEstilizado = styled.li`
-
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 22px;
+    color: ${ props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
 `
 
 function ItemNavegacao({children, iconeAtivo, iconeInativo, ativo = false}) {
     return (
-        <ItemNavegacaoEstilizado>
+        <ItemNavegacaoEstilizado $ativo={ativo}>
             <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
             {children}
         </ItemNavegacaoEstilizado>
--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -12,11 +12,37 @@ function BarraLateral() {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <ItemNavegacao>
-                        Home
+                    <ItemNavegacao
+                        iconeAtivo={'./icones/home-ativo.png'}
+                        iconeInativo={'./icones/home-inativo.png'}
+                        ativo = {true}
+                    >
+                        Início
                     </ItemNavegacao>
-                    <ItemNavegacao>
-                        Mais Vistos
+                    <ItemNavegacao
+                        iconeAtivo={'./icones/mais-vistas-ativo.png'}
+                        iconeInativo={'./icones/mais-vistas-inativo.png'}
+                    >
+                        Mais Vistas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={'./icones/mais-curtidas-ativo.png'}
+                        iconeInativo={'./icones/mais-curtidas-inativo.png'}
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={'./icones/novas-ativo.png'}
+                        iconeInativo={'./icones/novas-inativo.png'}
+
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={'./icones/surpreenda-me-ativo.png'}
+                        iconeInativo={'./icones/surpreenda-me-inativo.png'}
+                    >
+                        Surpreenda-me
                     </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
