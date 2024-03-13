5ee93803301c989d01ccbec048bb1a4f3be3f675
Ian Almeida <iangtaonline1@gmail.com>
2024-03-12 23:09:47 -0300

    Planejando Section 'galeria'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
 import Banner from "./componentes/Banner"
+import Galeria from "./componentes/Galeria"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);;
@@ -10,25 +11,37 @@ const FundoGradiente = styled.div`
   min-height: 100vh;
   margin-right: 60px;
 `
-const FlexContaienr = styled.div`
-  display: flex;
-  justify-content: space-between;
-  gap: 42px;
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
 `
-
+const MainContaienr = styled.main`
+  display: flex;
+  gap: 24px;
+`
+const ConteudoGaleria = styled.section`
+display: flex;
+flex-direction: column;
+flex-grow: 1;
+`
 function App() {
 
   return (
     <FundoGradiente>
-      <EstilosGlobais/>
-      <Cabecalho/>
-      <FlexContaienr>
-        <BarraLateral/>
-        <Banner 
-          texto={'A galeria mais completa de fotos do espaço!'}
-          backgroundImage={'./imagens/Banner.png'}
-        />
-      </FlexContaienr>
+      <EstilosGlobais />
+      <AppContainer>
+        <Cabecalho />
+        <MainContaienr>
+          <BarraLateral />
+          <ConteudoGaleria>
+            <Banner
+              texto={'A galeria mais completa de fotos do espaço!'}
+              backgroundImage={'./imagens/Banner.png'} />
+            <Galeria />
+          </ConteudoGaleria>
+        </MainContaienr>
+      </AppContainer>
     </FundoGradiente>
   )
 }
---
+++ src/componentes/Galeria/Tags/index.jsx
@@ -0,0 +1,5 @@
+function Tags() { 
+    return <p>Tags</p>
+}
+
+export default Tags
