f97d49b31080c8aa95cc1e327ae95cbd1e77a4c3
Ian Almeida <iangtaonline1@gmail.com>
2024-03-22 23:25:00 -0300

    alterando largura da imagem

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,19 +1,18 @@
 import { styled } from "styled-components"
 import CampoTexto from "../CampoTexto"
-import logo from '../../../public/imagens/logo.png'
 
 const HeaderEstilizado = styled.header`
     padding: 60px 0;
     display: flex;
     justify-content: space-between;
     img {
-        max-width: 252px;
+        width: 252px;
     }
 `
 
 const Cabecalho = ({setFiltroBusca}) => {
     return (<HeaderEstilizado>
-        <img src={logo} alt="" />
+        <img src='/imagens/logo.png' alt="" />
         <CampoTexto setFiltroBusca={setFiltroBusca}/>
     </HeaderEstilizado>)
 }
